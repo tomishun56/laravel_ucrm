bc174a9aad3e073c0ba6f1c5d0f28f91cb4ccc88
tomishun56 <tomisako.shunsuke@revamp.co.jp>
2023-06-08 20:00:33 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+</script>
+
+<template>
+<GuestLayout>
+    何か文字を書く
+    <InputLabel>件名</InputLabel>
+</GuestLayout>
+</template>
